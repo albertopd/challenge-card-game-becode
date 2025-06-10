db3fb0a4013bdecc00fb847bd430e4250b0875d1
Alberto <47937297+albertopd@users.noreply.github.com>
2025-06-10 15:45:37 +0200

    Add optional option to distribute an equal number of cards among all players

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from utils.player import Player
 from utils.game import Board
 
+# Create some players
 players = [Player("Alb"), Player("Sof"), Player("Evi")]
 
-board = Board(players)
+# Create a board and specify to distribute an equal number of cards among all players 
+board = Board(players, True)
+
 board.start_game()
